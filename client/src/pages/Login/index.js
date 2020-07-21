8c3b008d3368bd3db531e65c4d14833f294eb612
Connie Tran <connietrandev@gmail.com>
2020-07-21 19:08:41 -0400

    Create fetch for playlists on Home

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -4,6 +4,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import Button from 'react-bootstrap/Button';
+
 import './style.css';
 
 function Login() {
@@ -13,13 +15,13 @@ function Login() {
                 <Col>
                     <h1>Welcome to Playlistr</h1>
                     <h3>To continue, log in to Spotify</h3>
-                    <button onClick={() => {
+                    <Button variant="info" onClick={() => {
                         window.location = window.location.href.includes('localhost')
                             ? 'http://localhost:8888/spotify/login'
                             : 'https://playlistr-io.herokuapp.com/spotify/login'
                     }}>
                         Sign in with Spotify
-                </button>
+                </Button>
                 </Col>
             </Row>
         </Container>
