2c26326f64046fb35d26d616b1fa52506920eafc
John Herman <hermanjohn2@gmail.com>
2020-07-23 13:40:37 -0400

    Made changes to URL that is being sent to Room page. Edited Room page to be a stateful component. Once the component mounts we fetch current user data from Spotify. Using that data and the parsed url parameters, a collaborative playlist is created with Spotify.

--- client/src/pages/Room/index.js
+++ client/src/pages/Room/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import queryString from 'query-string';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -6,117 +6,162 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 
-const Room = () => {
-	let parsedUrl = queryString.parse(window.location.search);
-	let token = parsedUrl.access_token;
-	let userId = parsedUrl.user_id;
-	let roomId = parsedUrl.room_id;
+class Room extends Component {
+	constructor() {
+		super();
 
-	console.log('Token:');
-	console.log(token);
-	console.log('User ID:');
-	console.log(userId);
-	console.log('Room ID:');
-	console.log(roomId);
+		let parsedUrl = queryString.parse(window.location.search);
+		let token = parsedUrl.access_token;
+		let roomId = parsedUrl.room_id;
 
-	return (
-		<div>
-			<Container>
-				<Row>
-					<Col xs={5} md={6}>
-						<h1>Current Room: {roomId} </h1>
-					</Col>
-					<Col xs={4} md={4}>
-						<Button>Add</Button>
-						<input placeholder="Add a Track" />
-					</Col>
-					<Col xs={3} md={2}>
-						<Link to="/">
-							<Button className="float-right">Home</Button>
-						</Link>
-					</Col>
-				</Row>
-				<Row>
-					<Col xs={8} md={6}>
-						<div
-							style={{
-								height: '100%',
-								border: '1px solid black'
-							}}>
-							Artwork of Track Currently Playing
-						</div>
-					</Col>
-					<Col xs={4} md={6}>
-						<div
-							style={{
-								height: '100%',
-								border: '1px solid black'
-							}}>
-							<h1>Play Queue</h1>
-							<ol>
-								<li>Track</li>
-								<li>Track</li>
-								<li>Track</li>
-								<li>Track</li>
-								<li>Track</li>
-								<li>Track</li>
-								<li>Track</li>
-								<li>Track</li>
-							</ol>
-						</div>
-					</Col>
-				</Row>
-				<Row>
-					<Col xs={12} sm={6} md={6}>
-						<div>
-							<h1>Now Playing</h1>
+		this.state = {
+			user: {},
+			playlist: {},
+			accessToken: token,
+			roomId: roomId
+		};
+	}
+
+	// When component mounts, user state will be set to response from API call. Then the playlist will be created.
+	componentDidMount() {
+		fetch('https://api.spotify.com/v1/me', {
+			headers: {
+				Authorization: 'Bearer ' + this.state.accessToken
+			}
+		})
+			.then(res => res.json())
+			.then(data => {
+				console.log(data);
+				this.setState({ user: data });
+			})
+			.then(() => this.createPlaylist(this.state.user.id, this.state.roomId, this.state.accessToken));
+	}
+
+	createPlaylist = (user, room, token) => {
+		// console.log('UserId: ' + user);
+		console.log('Room: ' + room);
+		console.log('Token: ' + token);
+
+		fetch(`https://api.spotify.com/v1/users/${user}/playlists`, {
+			method: 'POST',
+			headers: {
+				Authorization: 'Bearer ' + token,
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				name: room,
+				public: false,
+				collaborative: true,
+				description: 'Playlistr: ' + room
+			})
+		})
+			.then(res => res.json())
+			.then(data => {
+				console.log(data);
+				this.setState({ playlist: data });
+			});
+	};
+
+	render() {
+		return (
+			<div>
+				<Container>
+					<Row>
+						<Col xs={5} md={6}>
+							<h1>Current Room: {this.state.roomId} </h1>
+						</Col>
+						<Col xs={4} md={4}>
+							<Button>Add</Button>
+							<input placeholder="Add a Track" />
+						</Col>
+						<Col xs={3} md={2}>
+							<Link to="/">
+								<Button className="float-right">Home</Button>
+							</Link>
+						</Col>
+					</Row>
+					<Row>
+						<Col xs={8} md={6}>
 							<div
 								style={{
-									height: '200px',
+									height: '100%',
 									border: '1px solid black'
 								}}>
-								Music Player
+								Artwork of Track Currently Playing
 							</div>
-						</div>
-					</Col>
-					<Col xs={12} sm={6} md={6}>
-						<Row className="pt-5">
-							<Col md={4} xs={4}>
+						</Col>
+						<Col xs={4} md={6}>
+							<div
+								style={{
+									height: '100%',
+									border: '1px solid black'
+								}}>
+								<h1>Play Queue</h1>
+								<ol>
+									<li>Track</li>
+									<li>Track</li>
+									<li>Track</li>
+									<li>Track</li>
+									<li>Track</li>
+									<li>Track</li>
+									<li>Track</li>
+									<li>Track</li>
+								</ol>
+							</div>
+						</Col>
+					</Row>
+					<Row>
+						<Col xs={12} sm={6} md={6}>
+							<div>
+								<h1>Now Playing</h1>
 								<div
 									style={{
-										height: '100px',
+										height: '200px',
 										border: '1px solid black'
 									}}>
-									Avatar
+									Music Player
 								</div>
-								<h2>USER</h2>
-							</Col>
-							<Col md={4} xs={4}>
-								<div
-									style={{
-										height: '100px',
-										border: '1px solid black'
-									}}>
-									Avatar
-								</div>
-								<h2>USER</h2>
-							</Col>
-							<Col md={4} xs={4}>
-								<div
-									style={{
-										height: '100px',
-										border: '1px solid black'
-									}}>
-									Avatar
-								</div>
-								<h2>USER</h2>
-							</Col>
-						</Row>
-					</Col>
-				</Row>
-			</Container>
-		</div>
-	);
-};
+							</div>
+						</Col>
+						<Col xs={12} sm={6} md={6}>
+							<Row className="pt-5">
+								<Col md={4} xs={4}>
+									<div
+										style={{
+											height: '100px',
+											border: '1px solid black'
+										}}>
+										Avatar
+									</div>
+									<h2>{this.state.user.display_name}</h2>
+								</Col>
+								<Col md={4} xs={4}>
+									<div
+										style={{
+											height: '100px',
+											border: '1px solid black'
+										}}>
+										Avatar
+									</div>
+									<h2>USER</h2>
+								</Col>
+								<Col md={4} xs={4}>
+									<div
+										style={{
+											height: '100px',
+											border: '1px solid black'
+										}}>
+										Avatar
+									</div>
+									<h2>USER</h2>
+								</Col>
+							</Row>
+						</Col>
+					</Row>
+				</Container>
+			</div>
+		);
+	}
+}
 
 export default Room;
