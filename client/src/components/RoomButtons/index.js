d30b13ed9e7186bbb0e3eedef0223b4f02fdf1b5
John Herman <hermanjohn2@gmail.com>
2020-08-01 21:35:27 -0400

    Edited Join Room button to inclue an input. Users can now enter the Room Id they would like to join. When join room is clicked the user will be sent to the Room Page that corresponds with the room id entered. Error handler adder in the event the input is empty or not 6 characters long.

--- client/src/components/RoomButtons/index.js
+++ client/src/components/RoomButtons/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import hexGen from 'hex-generator';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Alert from 'react-bootstrap/Alert';
+
+import './style.css';
+
 const RoomButtons = ({ token }) => {
+	// useState to set and store what the user room input
+	const [roomInput, setRoomInput] = useState('');
+
+	// useState for error handler
+	const [show, setShow] = useState('d-none');
+
 	// Outputs 6 character hex code that will serve as the Room Id
 	let roomHex = hexGen(24);
 
@@ -19,16 +33,44 @@ const RoomButtons = ({ token }) => {
 		}
 	};
 
-	return (
-		<div>
-			<button className="float-left">Join a Room</button>
+	// Verifies input is 6 digits and uses setUrl
+	const handleJoinRoom = e => {
+		e.preventDefault();
 
-			<Link to="/room">
-				<button onClick={() => setUrl(token, roomHex)} className="float-right">
+		// Verifies a user has put in a 6 digit room id
+		if (roomInput && roomInput.length === 6) setUrl(token, roomInput);
+		else {
+			setShow('');
+			setTimeout(() => setShow('d-none'), 3000);
+		}
+	};
+
+	return (
+		<Row>
+			<Col md={6} xs={8}>
+				<Form className="join-room-form">
+					<InputGroup>
+						<button className="join-room-btn" onClick={handleJoinRoom}>
+							Join a Room
+						</button>
+
+						<FormControl
+							className="room-input"
+							onChange={e => setRoomInput(e.target.value)}
+							value={roomInput}
+						/>
+					</InputGroup>
+				</Form>
+				<Alert variant="warning" className={show}>
+					Please enter a valid Room ID
+				</Alert>
+			</Col>
+			<Col md={6} xs={4}>
+				<button onClick={() => setUrl(token, roomHex)} className="float-right join-room-btn">
 					Create a Room
 				</button>
-			</Link>
-		</div>
+			</Col>
+		</Row>
 	);
 };
 
